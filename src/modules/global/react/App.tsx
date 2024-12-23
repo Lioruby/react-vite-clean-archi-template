9f8ee23dbac8619dfa07127853d4074dfe737664
Lior Levy <levy.lior.pro@gmail.com>
2024-12-23 17:18:05 +0100

    Task: fix assets

--- src/modules/global/react/App.tsx
+++ src/modules/global/react/App.tsx
@@ -1,20 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div data-testid="app">
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1 className="text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
